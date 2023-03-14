8740e70630f484cba078522a1dacd03f1240aebb
chrborakis <christos.borakis99@gmail.com>
2023-03-14 15:06:21 +0200

    cart thunk

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/ui-slice';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -17,41 +17,12 @@ function App() {
     const notification = useSelector( state => state.ui.notification);
 
     useEffect( () => {
-        const sendCartData = async () => {
-            dispatch(uiActions.showNotification({
-                status: "pending", 
-                title: "Sending", 
-                message: "Sending cart data..."
-            }));
-            const response = await fetch("https://shop-app-94cff-default-rtdb.europe-west1.firebasedatabase.app/cart.json",{
-                method: "PUT",
-                body: JSON.stringify(cart)
-            });
-
-            if( !response.ok) throw new Error("Sending cart data failed!");
-
-            dispatch(uiActions.showNotification({
-                status: "success", 
-                title: "Success!", 
-                message: "Sent cart data successfully!"
-            }));
-
-            const responseData = await response.json();
-        };
-
-        if( isInitial) {
+        if( isInitial){
             isInitial = false;
             return;
-        };
-
-        sendCartData().catch( (error) => {
-            dispatch(uiActions.showNotification({
-                status: "error", 
-                title: "Error!", 
-                message: "Sending cart data failed!"
-            }));
-        });
-    }, [cart, dispatch]);
+        }
+        dispatch(sendCartData(cart));
+    }, [cart,dispatch]);
 
     return (<Fragment>
         {notification && (
